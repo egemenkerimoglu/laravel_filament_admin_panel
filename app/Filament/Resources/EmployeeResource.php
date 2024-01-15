72d2419d84237c887127f99af7cc1b0a0e38a97a
egemenkerimoglu <egemen@elkomyazilim.com>
2024-01-15 13:59:49 +0300

    create employees

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -95,8 +95,12 @@ class EmployeeResource extends Resource
                 Forms\Components\Section::make('Dates')
                     ->schema([
                         Forms\Components\DatePicker::make('date_of_birth')
+                            // ->native(false) // Zaman Formatını Özelleştirme
+                            ->displayFormat('d/m/Y')
                             ->required(),
                         Forms\Components\DatePicker::make('date_hired')
+                            ->native(false)
+                            ->displayFormat('d/m/Y')
                             ->required(),
                     ])->columns(2),
                 Forms\Components\TextInput::make('country_id')
